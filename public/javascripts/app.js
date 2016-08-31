2804519ece518e85f04435a49d0b2aa1a85f1b88
colin1717 <colin1717@mac.com>
2016-08-31 15:16:16 -0500

    angular linking to shopping page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,18 @@
-angular.module('helm', []);
+angular
+    .module('helm', ['ui.router'])
+    .config(MainRouter);
+
+  MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function MainRouter($stateProvider, $urlRouterProvider){
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: '../templates/home.html'
+      })
+      .state('shop', {
+        url: '/shop',
+        templateUrl: '../templates/shop.html'
+      })
+
+      $urlRouterProvider.otherwise('/');
+  }
